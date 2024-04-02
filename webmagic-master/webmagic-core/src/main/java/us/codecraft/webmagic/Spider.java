0cba90849b987ebf58eb4f1efcd20b06cdaad317
Fondeur Renaud <renaud.fondeur.etu@a12p16.fil.univ-lille.fr>
2024-04-02 17:47:17 +0200

    supression de spécification de type inutile

--- webmagic-master/webmagic-core/src/main/java/us/codecraft/webmagic/Spider.java
+++ webmagic-master/webmagic-core/src/main/java/us/codecraft/webmagic/Spider.java
@@ -66,7 +66,7 @@ public class Spider implements Runnable, Task {
 
     protected Downloader downloader;
 
-    protected List<Pipeline> pipelines = new ArrayList<Pipeline>();
+    protected List<Pipeline> pipelines = new ArrayList<>();
 
     protected PageProcessor pageProcessor;
 
@@ -251,7 +251,7 @@ public class Spider implements Runnable, Task {
      * @return this
      */
     public Spider clearPipeline() {
-        pipelines = new ArrayList<Pipeline>();
+        pipelines = new ArrayList<>();
         return this;
     }
 
