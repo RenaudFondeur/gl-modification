7f7b55a9d1e4f10c157198af3a645964511d4dfd
Renaud <renaud.fondeur.etu>
2024-04-06 20:20:01 +0200

    denestage de classe

--- webmagic-master/webmagic-core/src/main/java/us/codecraft/webmagic/selector/SmartContentSelector.java
+++ webmagic-master/webmagic-core/src/main/java/us/codecraft/webmagic/selector/SmartContentSelector.java
@@ -16,59 +16,58 @@ import java.util.List;
 @Experimental
 public class SmartContentSelector implements Selector {
 
-    public SmartContentSelector() {
-    }
-
-    @Override
-    public String select(String html) {
+    public String processHTML(String html) {
         html = html.replaceAll("(?is)<!DOCTYPE.*?>", "");
         html = html.replaceAll("(?is)<!--.*?-->", "");				// remove html comment
         html = html.replaceAll("(?is)<script.*?>.*?</script>", ""); // remove javascript
         html = html.replaceAll("(?is)<style.*?>.*?</style>", "");   // remove css
         html = html.replaceAll("&.{2,5};|&#.{2,5};", " ");			// remove special char
         html = html.replaceAll("(?is)<.*?>", "");
-        List<String> lines;
-        int blocksWidth =3;
-        int threshold =86;
-        int start;
-        int end;
-        StringBuilder text = new StringBuilder();
-        ArrayList<Integer> indexDistribution = new ArrayList<Integer>();
+        return html;
+    }
 
-        lines = Arrays.asList(html.split("\n"));
-
+    public ArrayList<Integer> initIndexDistribution(List<String> lines, int blocksWidth) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < lines.size() - blocksWidth; i++) {
             int wordsNum = 0;
             for (int j = i; j < i + blocksWidth; j++) {
                 lines.set(j, lines.get(j).replaceAll("\\s+", ""));
                 wordsNum += lines.get(j).length();
             }
-            indexDistribution.add(wordsNum);
+            result.add(wordsNum);
         }
+        return result;
+    }
 
-        start = -1; end = -1;
-        boolean boolstart = false, boolend = false;
+    @Override
+    public String select(String html) {
+        html = processHTML(html);
+        StringBuilder text = new StringBuilder();
         text.setLength(0);
+        
+        List<String> lines = Arrays.asList(html.split("\n"));
+        int blocksWidth =3;
+        ArrayList<Integer> indexDistribution = initIndexDistribution(lines, blocksWidth);
+
+        int start = -1;
+        int end = -1;
+        int threshold = 86;
+
+        boolean boolstart = false;
+        boolean boolend = false;
 
         for (int i = 0; i < indexDistribution.size() - 1; i++) {
-            if (indexDistribution.get(i) > threshold && ! boolstart) {
-                if (indexDistribution.get(i+1).intValue() != 0
-                        || indexDistribution.get(i+2).intValue() != 0
-                        || indexDistribution.get(i+3).intValue() != 0) {
-                    boolstart = true;
-                    start = i;
-                    continue;
-                }
+            if (indexDistribution.get(i) > threshold && ! boolstart && (indexDistribution.get(i+1).intValue() != 0 || indexDistribution.get(i+2).intValue() != 0 || indexDistribution.get(i+3).intValue() != 0)) {
+                boolstart = true;
+                start = i;
+                continue;
             }
-            if (boolstart) {
-                if (indexDistribution.get(i).intValue() == 0
-                        || indexDistribution.get(i+1).intValue() == 0) {
-                    end = i;
-                    boolend = true;
-                }
+            if (boolstart && (indexDistribution.get(i).intValue() == 0 || indexDistribution.get(i+1).intValue() == 0)) {
+                end = i;
+                boolend = true;
             }
-            StringBuilder tmp = new StringBuilder();
             if (boolend) {
+                StringBuilder tmp = new StringBuilder();
                 for (int ii = start; ii <= end; ii++) {
                     if (lines.get(ii).length() < 5) continue;
                     tmp.append(lines.get(ii) + "\n");
