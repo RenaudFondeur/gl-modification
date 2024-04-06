7da7768ae87c2950e88398e1e89c50017af38046
Renaud <renaud.fondeur.etu>
2024-04-06 18:56:37 +0200

    addAll au lieu de add dans un for

--- webmagic-master/webmagic-core/src/main/java/us/codecraft/webmagic/utils/WMCollections.java
+++ webmagic-master/webmagic-core/src/main/java/us/codecraft/webmagic/utils/WMCollections.java
@@ -1,6 +1,7 @@
 package us.codecraft.webmagic.utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,17 +15,13 @@ public class WMCollections {
 
     public static <T> Set<T> newHashSet(T... t){
         Set<T> set = new HashSet<T>(t.length);
-        for (T t1 : t) {
-            set.add(t1);
-        }
+        Collections.addAll(set, t);
         return set;
     }
 
     public static <T> List<T> newArrayList(T... t){
         List<T> list = new ArrayList<T>(t.length);
-        for (T t1 : t) {
-            list.add(t1);
-        }
+        Collections.addAll(list, t);
         return list;
     }
 }
